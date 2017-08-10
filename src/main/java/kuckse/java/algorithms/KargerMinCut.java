56065d8dc2b6f51800bde17b3ce8c442f9935191
Eric Kucks <Eric Kucks>
2017-08-10 08:15:09 -0400

    implementing getGraph

--- src/main/java/kuckse/java/algorithms/KargerMinCut.java
+++ src/main/java/kuckse/java/algorithms/KargerMinCut.java
@@ -1,6 +1,22 @@
 package kuckse.java.algorithms;
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+
 public class KargerMinCut {
+    public int calculate(Graph graph) {
+        int numVertices = graph.numVertices;
+        final Random random = new Random();
+        while(numVertices > 2) {
+            random.nextInt(graph.numEdges);
+        }
+        return 0;
+    }
+
     public static class Edge {
         public int src;
         public int dest;
@@ -9,9 +25,31 @@ public class KargerMinCut {
             this.src = src;
             this.dest = dest;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Edge edge = (Edge) o;
+            return src == edge.src &&
+                    dest == edge.dest;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(src, dest);
+        }
     }
 
     public static class Graph {
-        
+        public List<Edge> edges;
+        public int numEdges;
+        public int numVertices;
+
+        public Graph(List<Edge> edges, int numVertices) {
+            this.edges = edges;
+            this.numEdges = edges.size();
+            this.numVertices = numVertices;
+        }
     }
 }
--- src/test/java/kuckse/java/algorithms/KargerMinCutTest.java
+++ src/test/java/kuckse/java/algorithms/KargerMinCutTest.java
@@ -1,31 +1,54 @@
 package kuckse.java.algorithms;
 
+import com.google.common.collect.Sets;
+import com.googlecode.totallylazy.Lists;
+import kuckse.java.algorithms.KargerMinCut.Edge;
+import kuckse.java.algorithms.KargerMinCut.Graph;
 import org.apache.commons.io.FileUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class KargerMinCutTest {
-    private KargerMinCut minCut = new KargerMinCut();
+    private KargerMinCut minCutCalculator = new KargerMinCut();
 
     @Test
     public void testSmall1() throws Exception {
         //given
-        Map<Integer, int[]> graph = getGraph();
+        Graph graph = getGraph();
 
         //when
-
+        int minCut = minCutCalculator.calculate(graph);
 
         //then
-
+       assertThat(minCut, equalTo(2));
     }
 
-    private Map<Integer, int[]> getGraph() throws IOException {
-        final List<String> s = FileUtils.readLines(new ClassPathResource("kargerMinCut.txt").getFile(), Charset.defaultCharset());
-        return null;
+    private Graph getGraph() throws IOException {
+        final List<String> adjacentVerticesString = FileUtils.readLines(new ClassPathResource("kargerMinCut.txt").getFile(), Charset.defaultCharset());
+        final Set<HashSet<Integer>> rawEdges = sequence(adjacentVerticesString)
+                .map(s -> s.split("\\s+"))
+                .map(ss -> sequence(ss).map(Integer::parseInt))
+                .flatMap(is -> is.drop(1).map(adjacent -> newHashSet(adjacent, is.first())))
+                .toSet();
+        final List<Edge> edges = sequence(rawEdges)
+                .map(Set::iterator)
+                .map(edgeIterator -> new Edge(edgeIterator.next(), edgeIterator.next()))
+                .toList();
+
+        return new Graph(edges, adjacentVerticesString.size());
     }
 }
